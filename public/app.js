4ba59ad22faa97cf17d49301d2a02041adc2fdc0
Jose <josemanuel21302@gmail.com>
2023-02-09 20:42:22 -0800

    added map with leaftlet

--- public/app.js
+++ public/app.js
@@ -4,28 +4,43 @@
 
 // Create map
 
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 
 
 // Add openstreetmap tiles
-
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);    
 
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
 
-// Create and add a geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([51.5, -0.09])
+marker.addTo(map).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 
 
 // Draw the 2nd arrondissement
-
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
 
 
 // Metro station markers
-
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
 
 
 
